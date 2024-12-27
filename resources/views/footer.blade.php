311b8c781d45a6ada4cda05c073a4f7c87277a1d
mousmi12 <mousminishad@gmail.com>
2024-12-27 20:30:45 +0530

    Edited Signed-off-by: mousmi12 <mousminishad@gmail.com>

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,122 @@
+<footer class="footer-section">
+        <div class="container row">
+          <div class="col-md-6 col-12">
+            <div>
+              <div class="logo-image">
+                <a href="#top-section">
+                  <img
+                    src="{{asset('assets/images/LOGO.png')}}"
+                    alt=""
+                    class="navbarimg"
+                    srcset=""
+                  />
+                </a>
+              </div>
+              <div>
+                <p class="footerparagraphwidth">
+                Fastride Shipping LLC is one of Dubai’s leading shipping and logistics companies, providing comprehensive freight forwarding solutions across the UAE and beyond.
+                </p>
+              </div>
+              <!-- <div class="icons-colors ">
+                <a
+                  href="https://www.facebook.com/photo/?fbid=122110908794423546&set=a.122100275210423546"
+                  target="_blank"
+                >
+                  <i class="bi bi-facebook"></i>
+                </a>
+                <a
+                  href="https://www.instagram.com/p/C_U0PhOyxR0/."
+                  target="_blank"
+                  ><i class="bi bi-instagram"></i
+                ></a> -->
+              <!-- <i class="bi bi-twitter-x"></i> -->
+              <!-- <a
+                  href="https://www.linkedin.com/company/exim-clearing-and-forwarding-l-l-c-dubai/?viewAsMember=true"
+                  target="_blank"
+                >
+                  <i class="bi bi-linkedin"></i>
+                </a> -->
+              <!-- <i class="bi bi-twitter"></i> -->
+              <!-- </div>-->
+            </div>
+          </div>
+          <div class="col-md-6 col-12">
+            <div class="center-align">
+              <!-- <div class="get-in-touch">
+                <h1>Get In Touch</h1>
+              </div> -->
+              <div>
+                <ul>
+                  <li>
+                    <div>
+                      <a href="#">
+                        <i class="bi bi-geo-alt-fill"></i>
+                      </a>
+                    </div>
+                    <div>
+                      <a href="#">
+                      P O Box 183823, Al Khabaisi, Deira, Dubai, UAE
+                        <br />
+
+                      </a>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <a href="tel:+971 4282 3411">
+                        <i class="bi bi-phone-fill"></i>
+                      </a>
+                    </div>
+                    <div><a href="tel:+91995559990">+971552178868, +97142828570, +97142820697</a></div>
+                  </li>
+                  <li>
+                    <div>
+                      <a class="mailto" href="mailto: info@fastride.com">
+                        <i class="bi bi-envelope-fill"></i>
+                      </a>
+                    </div>
+                    <div>
+                      <a class="mailto" href="mailto:info@exim.ae">
+                      enquiry@fastride.ae
+                      </a>
+                    </div>
+                  </li>
+                  <!-- <li>
+                    <div>
+                      <a href="www.exim.ae" target="_blank">
+                        <i class="bi bi-globe"></i>
+                      </a>
+                    </div>
+                    <div>
+                      <a href="www.exim.ae" target="_blank">www.exim.ae</a>
+                    </div>
+                  </li> -->
+                  <li>
+                    <div class="icons-colors">
+                      <a
+                        href="https://www.facebook.com/share/1EnwG1qtYC/"
+                        target="_blank"
+                      >
+                        <i class="bi bi-facebook"></i>
+                      </a>
+                      <a
+                        href="https://www.instagram.com/fastrideshipping/"
+                        target="_blank"
+                        ><i class="bi bi-instagram"></i
+                      ></a>
+                      <!-- <i class="bi bi-twitter-x"></i> -->
+                      <a
+                        href="https://ae.linkedin.com/company/fastride-shipping-llc"
+                        target="_blank"
+                      >
+                        <i class="bi bi-linkedin"></i>
+                      </a>
+                      <!-- <i class="bi bi-twitter"></i> -->
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
---
+++ resources/views/navmenu.blade.php
@@ -0,0 +1,76 @@
+
+
+
+<section class="bg-white col-12 position-fixed z-3">
+        <nav class="navbar navbar-expand-lg bg-white container-fluid">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#"
+              ><img
+               src="{{asset('assets/images/LOGO.png')}}"
+                class="navbarimg"
+                alt=""
+                srcset=""
+            /></a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavDropdown"
+              aria-controls="navbarNavDropdown"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul class="navbar-nav ms-auto">
+                <li class="nav-item m-auto">
+                  <a
+                    class="nav-link navbarfontColorSize"
+                    aria-current="page"
+                    href="#"
+                    >Home</a
+                  >
+                </li>
+                <li class="nav-item m-auto">
+                  <a class="nav-link navbarfontColorSize" href="#"
+                    >Who We Are</a
+                  >
+                </li>
+                <li class="nav-item m-auto">
+                  <a class="nav-link navbarfontColorSize" href="#">Services</a>
+                </li>
+                <li class="nav-item m-auto">
+                  <a class="nav-link navbarfontColorSize" href="#">DG Cargo</a>
+                </li>
+                <li class="nav-item m-auto">
+                  <a class="nav-link navbarfontColorSize" href="#">Connect With Us</a>
+                </li>
+                <li class="nav-item whatsappColor m-3">
+                  <a class="navbar-brand text-white" href="#"
+                    ><i class="bi bi-whatsapp whatsappColorfontsize"></i
+                  ></a>
+                </li>
+                <!-- <li class="nav-item dropdown">
+                      <a
+                        class="nav-link dropdown-toggle"
+                        href="#"
+                        role="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                      >
+                        Dropdown link
+                      </a>
+                      <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li>
+                          <a class="dropdown-item" href="#">Something else here</a>
+                        </li>
+                      </ul>
+                    </li> -->
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </section>
